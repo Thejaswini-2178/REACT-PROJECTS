255c221506de4b75daa787d291af64f2c35717ec
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-04-18 20:23:23 +0530

    React-JOBTRAKER

---
+++ REACT-PROJECT/job-tracker/src/components/Filters.jsx
@@ -0,0 +1,17 @@
+export default function Filters({ statusFilter, setStatusFilter }) {
+    return (
+      <div className="filters">
+        <label>Filter by Status:</label>
+        <select
+          value={statusFilter}
+          onChange={(e) => setStatusFilter(e.target.value)}
+        >
+          <option value="All">All</option>
+          <option value="Applied">Applied</option>
+          <option value="Interview">Interview</option>
+          <option value="Offer">Offer</option>
+          <option value="Rejected">Rejected</option>
+        </select>
+      </div>
+    );
+  }
