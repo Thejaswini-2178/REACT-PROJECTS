11d12b47574dea409bc0e067b06ef68f9e79f7d5
Thejaswini-2178 <thejaswini2178@gmail.com>
2025-04-22 22:21:59 +0530

    React-Budgeting App

--- REACT-PROJECT/budgetting App/budget-app/src/components/Card.jsx
+++ REACT-PROJECT/budgetting App/budget-app/src/components/Card.jsx
@@ -1,23 +1,23 @@
 const Card = ({ title, value, icon, iconClass, change, changePositive }) => {
-    return (
-      <div className={`card fade-in`}>
-        <div className="card-header">
-          <div className="card-title">{title}</div>
-          <div className={`card-icon ${iconClass}`}>
-            {icon}
-          </div>
-        </div>
-        <div className="card-value">{value}</div>
-        <div className={`card-change ${changePositive ? 'positive' : 'negative'}`}>
-          {changePositive ? (
-            <FaArrowUp className="change-icon" />
-          ) : (
-            <FaArrowDown className="change-icon" />
-          )}
-          {change}
+  return (
+    <div className={`card fade-in`}>
+      <div className="card-header">
+        <div className="card-title">{title}</div>
+        <div className={`card-icon ${iconClass}`}>
+          {icon}
         </div>
       </div>
-    );
-  };
-  
-  export default Card;
+      <div className="card-value">{value}</div>
+      <div className={`card-change ${changePositive ? 'positive' : 'negative'}`}>
+        {changePositive ? (
+          <FaArrowUp className="change-icon" />
+        ) : (
+          <FaArrowDown className="change-icon" />
+        )}
+        {change}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
